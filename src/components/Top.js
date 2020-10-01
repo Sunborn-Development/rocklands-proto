b390ba89aa1a7f0649bf56681fa057d77257fd89
Jin Kimura <kimura.jin@sunborn.co.jp>
2020-10-01 18:59:56 +0900

    キャンペーンバナー追加 #3

--- src/components/Top.js
+++ src/components/Top.js
@@ -3,9 +3,9 @@ import React from 'react'
 import pdf from '../assets/files/cp.202010.pdf'
 
 const Top = () => (
-  <div id="top">
-  【10月はWelcomキャンペーン月間！】<span>新規ご来店の方、新規の方を連れて来てくださった会員の方、どちらもとってもお得！</span><a href={pdf} target="_blank">詳細はこちら＞</a>
-  </div>
+<div id="top">
+<a href={pdf} target="_blank" rel="noreferer">【10月はWelcomキャンペーン月間！】<span>新規ご来店の方、新規の方を連れて来てくださった会員の方、どちらもとってもお得！</span>詳細はこちら＞</a>
+</div>
 )
 
 export default Top
