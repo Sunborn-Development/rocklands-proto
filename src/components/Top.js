853bec102d60fe739fc0e47e27b94b8bf2e9862c
Jin Kimura <kimura.jin@sunborn.co.jp>
2020-11-02 13:40:24 +0900

    202011 キャンペーン更新

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-import pdf from '../assets/files/cp.202010.pdf'
+//import pdf from '../assets/files/cp.202010.pdf'
+import pdf from '../assets/files/cp.202011.pdf'
 
 const Top = () => (
 <div id="top">
-<a href={pdf} target="_blank" rel="noreferer">【10月はWelcomキャンペーン月間！】<span>新規ご来店の方、新規の方を連れて来てくださった会員の方、どちらもとってもお得！</span>詳細はこちら＞</a>
+<a href={pdf} target="_blank" rel="noreferer">【11月も継続中！Welcomeキャンペーン】<span>新規ご来店の方、新規の方を連れてきてくださった会員の方、どちらもとってもお得！</span>詳細はこちら＞</a>
 </div>
 )
 
