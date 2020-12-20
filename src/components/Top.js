643be60f4d2e708e6f6c7ce65f93631fb620b305
Jin Kimura <kimura.jin@sunborn.co.jp>
2020-12-20 13:57:07 +0900

    バナー調整

--- src/components/Top.js
+++ src/components/Top.js
@@ -6,7 +6,7 @@ import pdf from '../assets/files/cp.202012.pdf'
 
 const Top = () => (
 <div id="top">
-<a href={pdf} target="_blank" rel="noreferer">【どっちもお得！初めてのご来店/初めての方とご同伴】<span>Welcom to the ROCKLANDSプログラム！詳細はこちら</span>＞</a>
+<a href={pdf} target="_blank" rel="noreferer">【みんなお得！初来店の方 / ご同伴の方】<span>Welcom to the ROCKLANDSプログラム！詳細はこちら</span>＞</a>
 </div>
 )
 
