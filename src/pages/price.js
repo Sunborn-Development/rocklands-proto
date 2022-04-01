e5a2f4ae9adb02e779b703d1ed1af6c560db1892
kitahara.gou <kitahara.gou@sunborn.co.jp>
2022-04-01 13:22:00 +0900

    Update price.js

--- src/pages/price.js
+++ src/pages/price.js
@@ -18,7 +18,7 @@ const Landing = (props) => (
     <section id="one">
       <div className="inner">
         <p className="small">
-          料金はすべて税込（10%）
+          料金はすべて税込（10%） / 2022.4.1更新
         </p>
         <hr />
         <header className="major">
