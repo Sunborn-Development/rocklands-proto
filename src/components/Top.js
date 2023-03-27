69eed37b5e708cf096c90fb25fd3bcc0c2b67fbe
Jin Kimura <kimura.jin@sunborn.co.jp>
2023-03-27 10:15:27 +0900

    CP PDF log

--- src/components/Top.js
+++ src/components/Top.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 //import pdf from '../assets/files/cp.202010.pdf'
 //import pdf from '../assets/files/cp.202011.pdf'
-import pdf from '../assets/files/cp.202012.pdf'
+//import pdf from '../assets/files/cp.202012.pdf'
 
 const Top = () => (
 <div id="top">
-<a href={pdf} target="_blank" rel="noreferer">【みんなお得！初来店の方 / ご同伴の方】<span>Welcom to the ROCKLANDSプログラム！</span>詳細はこちら＞</a>
 </div>
 )
 
 export default Top
+
+
+/*
+<a href={pdf} target="_blank" rel="noreferer">【みんなお得！初来店の方 / ご同伴の方】<span>Welcom to the ROCKLANDSプログラム！</span>詳細はこちら＞</a>
+*/
